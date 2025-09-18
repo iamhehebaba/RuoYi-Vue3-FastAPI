f9b64d515460346bbaf2327be593790e68e0c477
galen <galen.sh@hotmail.com>
2025-09-18 10:24:14 +0800

    remove route for model_controller & proxy_controller

--- ruoyi-fastapi-backend/server.py
+++ ruoyi-fastapi-backend/server.py
@@ -23,9 +23,7 @@ from module_admin.controller.role_controller import roleController
 from module_admin.controller.server_controller import serverController
 from module_admin.controller.user_controller import userController
 from module_generator.controller.gen_controller import genController
-from module_admin.controller.proxy_controller import proxyController
 from module_admin.controller.agent_controller import agentController
-from module_admin.controller.model_controller import modelController
 from module_admin.controller.ragflow_model_controller import ragflowModelController
 
 from sub_applications.handle import handle_sub_applications
@@ -86,9 +84,7 @@ controller_list = [
     {'router': commonController, 'tags': ['通用模块']},
     {'router': genController, 'tags': ['代码生成']},
     {'router': agentController, 'tags': ['智能体管理']},
-    {'router': modelController, 'tags': ['模型管理']},
     {'router': ragflowModelController, 'tags': ['Ragflow模型管理']},
-    {'router': proxyController, 'tags': ['代理模块']},
 ]
 
 for controller in controller_list:
